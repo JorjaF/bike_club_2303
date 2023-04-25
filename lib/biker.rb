d8b8b19804ad10f397110216b42c8ff24944a0b5
Jorja Fleming <aset284@gmail.com>
2023-04-25 11:56:56 -0600

    Ugh

--- lib/biker.rb
+++ lib/biker.rb
@@ -3,23 +3,23 @@ class Biker
   attr_reader :name,
               :max_distance,
               :rides,
-              :acceptable_terrain,
-              :time
+              :acceptable_terrain
+              
 
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
     @rides = {}
     @acceptable_terrain = []
-    @time = time
+    
   end
 
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
 
-  def log_ride(time)
-    return nil unless @acceptable_terrain.include?(@acceptable_terrain)
+  def log_ride(rides)
+    return nil unless @acceptable_terrain.include?(terrain)
     if total_distance > @max_distance
       nil
     else
